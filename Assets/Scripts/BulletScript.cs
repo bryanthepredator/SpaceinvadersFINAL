d8e4a2d437aaa2405838fd41732a3c5f73d0e194
bryanthepredator <61429926+bryanthepredator@users.noreply.github.com>
2020-12-10 21:02:39 -0500

    Here;s some changes

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    public GameObject bullet;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,14 @@ public class BulletScript : MonoBehaviour
     void Update()
     {
         transform.Translate(new Vector3(0, -5 * Time.deltaTime, 0));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(bullet);
+        }
     }
 }
