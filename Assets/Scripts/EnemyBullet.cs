280cf9fded17ced3c3f97a6ba85f1aa5d8bd34ff
bryanthepredator <61429926+bryanthepredator@users.noreply.github.com>
2020-12-11 00:53:49 -0500

    Changes, Player shooting and enemy shooting

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public GameObject enemyBullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, -5 * Time.deltaTime, 0));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            Destroy(enemyBullet);
+        }
+    }
+}
--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    float timer = 0;
+    float timeToMove = 0.5f;
+    int numOfMovements = 0;
+    float speed = 0.25f;
+
+
+    public GameObject enemy;
+    public GameObject enemyBullet; 
+    public GameObject enemyBulletClone;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,30 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if(timer > timeToMove && numOfMovements < 3)
+        {
+            transform.Translate(new Vector3(speed, 0, 0));
+            timer = 0;
+            numOfMovements++;
+         
+        }
+        if(numOfMovements == 3)
+        {
+            transform.Translate(new Vector3(0, -0.5f, 0));
+            numOfMovements = -1;
+            speed = -speed;
+            timer = 0;
+        }
+
+        fireEnemyBullet();
+    }
+
+    void fireEnemyBullet()
+    {
+        if (Random.Range(0f, 500f) < 1)
+        {
+            enemyBulletClone = Instantiate(enemyBullet, new Vector3(enemy.transform.position.x, enemy.transform.position.y - 0.4f, 0), transform.rotation) as GameObject;
+        }
     }
 }
